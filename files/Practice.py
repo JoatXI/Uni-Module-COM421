cbbfbc78d1fc36587aea781465fa1ba7c032518b
Michael Paterson <michaelpaterson194@gmail.com>
2022-11-09 18:41:49 +0000

    _

--- files/Practice.py
+++ files/Practice.py
@@ -1,33 +1,44 @@
 class student:
-    def __init__(self, id, name, course, mark):
+    def __init__(self, id, name, course):
         self.id = id
         self.name = name
         self.course = course
-        self.mark = mark
-
-    def set_mark(self, mark):
-            if mark_input >= 0 and mark_input <= 100:
-                return True
-            else:
-                return False
-
-    def get_grade(self):
-        if mark_input >= 70:
-            print("First")
-        elif mark_input >= 60 and mark_input <= 69:
-            print("2/1")
-        elif mark_input >= 50 and mark_input <= 59:
-            print("2/2")
-        elif mark_input >= 40 and mark_input <= 49:
-            print("Third")
-        else:
-            print("Fail")
+        self.mark = 0
         
-
+# Defining a __str__ method that returns the complete student details as a string.
     def __str__(self):
         return f"{self.id}, {self.name}, {self.course}, {self.mark}"
 
+    def set_mark(self):
+        if mark_input >= 0 and mark_input <= 100:
+            return True
+        else:
+            return False
 
+# Defining a method that returns the students' grades as a string based on the mark.
+    def get_grade():
+        if mark_input >= 70:
+            return "First"
+        elif mark_input >= 60 and mark_input <= 69:
+            return "2/1"
+        elif mark_input >= 50 and mark_input <= 59:
+            return "2/2"
+        elif mark_input >= 40 and mark_input <= 49:
+            return "Third"
+        else:
+            return "Fail"
+
+# Returning a boolean indicating whether the mark is above or below the 40.            
+    def did_pass():
+        if mark_input >= 40:
+            return True
+        else:
+            return False
+        
+    print(get_grade())
+    print(did_pass())
+
+# Creating a list of students/pupils.
 pupils = []
 for num in range(5):
     id_input = int(input("Enter student id: "))
@@ -40,3 +51,4 @@ for num in range(5):
     
 for x in pupils:
     print(x)
+    
