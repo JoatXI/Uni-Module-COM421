d7bcb0a173c590adc22950dd32692fe06f58ccd3
Michael Paterson <michaelpaterson194@gmail.com>
2022-11-10 12:59:16 +0000

    _

--- files/Practice.py
+++ files/Practice.py
@@ -9,34 +9,34 @@ class student:
     def __str__(self):
         return f"{self.id}, {self.name}, {self.course}, {self.mark}"
 
-    def set_mark(self):
+    def set_mark(self, mark_input):
         if mark_input >= 0 and mark_input <= 100:
+            self.mark = mark_input
             return True
         else:
             return False
 
 # Defining a method that returns the students' grades as a string based on the mark.
-    def get_grade():
-        if mark_input >= 70:
+    def get_grade(self):
+        if self.mark >= 70:
             return "First"
-        elif mark_input >= 60 and mark_input <= 69:
+        elif self.mark >= 60 and self.mark <= 69:
             return "2/1"
-        elif mark_input >= 50 and mark_input <= 59:
+        elif self.mark >= 50 and self.mark <= 59:
             return "2/2"
-        elif mark_input >= 40 and mark_input <= 49:
+        elif self.mark >= 40 and self.mark <= 49:
             return "Third"
         else:
             return "Fail"
 
 # Returning a boolean indicating whether the mark is above or below the 40.            
-    def did_pass():
-        if mark_input >= 40:
+    def did_pass(self):
+        if self.mark >= 40:
             return True
         else:
             return False
         
-    print(get_grade())
-    print(did_pass())
+    
 
 # Creating a list of students/pupils.
 pupils = []
@@ -48,6 +48,9 @@ for num in range(5):
 
     student1 = student(id_input, name_input, course_input, mark_input)
     pupils.append(student1)
+    
+    print(student1.get_grade())
+    print(student1.did_pass())
     
 for x in pupils:
     print(x)
